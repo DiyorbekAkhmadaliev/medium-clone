626834d62e5acbea98eca1c80bdf8362e92bf62a
Diyorbek <diorakhmadaliev@gmail.com>
2023-05-09 16:48:20 +0500

    Article CRUD started22

--- src/main/java/uz/nt/mediumclone/service/impl/ArticleServicesImpl.java
+++ src/main/java/uz/nt/mediumclone/service/impl/ArticleServicesImpl.java
@@ -1,13 +1,22 @@
 package uz.nt.mediumclone.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import uz.nt.mediumclone.controller.ArticleResources;
 import uz.nt.mediumclone.dto.ArticlesDto;
+import uz.nt.mediumclone.repository.ArticleRepository;
 import uz.nt.mediumclone.service.ArticleServices;
 
+@Service
+@RequiredArgsConstructor
 public class ArticleServicesImpl implements ArticleServices {
+
+    private final ArticleRepository articleRepository;
+
     @Override
     public ResponseEntity<ArticlesDto> addArticle(ArticlesDto articlesDto) {
-
+        articleRepository.findById(articlesDto.getId());
         return null;
     }
 

9f4bb3317271509d999100f8719c89208ed39af6
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-05-10 18:13:24 +0500

    change UserEntity

--- src/main/java/uz/nt/mediumclone/service/mapper/UserMapper.java
+++ src/main/java/uz/nt/mediumclone/service/mapper/UserMapper.java
@@ -3,8 +3,8 @@ package uz.nt.mediumclone.service.mapper;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 import uz.nt.mediumclone.dto.UserDto;
-import uz.nt.mediumclone.model.UserEntity;
+import uz.nt.mediumclone.model.User;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends CommonMapper<UserDto, UserEntity> {
+public interface UserMapper extends CommonMapper<UserDto, User> {
 }

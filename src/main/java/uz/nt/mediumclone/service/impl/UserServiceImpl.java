eadf5ea4a8587519f9c58365732414900d52d895
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-05-16 16:20:04 +0500

    Complete UserServic

--- src/main/java/uz/nt/mediumclone/service/impl/UserServiceImpl.java
+++ src/main/java/uz/nt/mediumclone/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import uz.nt.mediumclone.security.JwtService;
 import uz.nt.mediumclone.dto.FollowDto;
